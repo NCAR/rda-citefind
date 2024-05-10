2bccb03e34e7d5f147280b1aaf576ad2d7757a38
rda-dattore <dattore@ucar.edu>
2024-05-10 09:10:46 -0600

    add new checks

--- src/libcitefind/integrity_checks.cpp
+++ src/libcitefind/integrity_checks.cpp
@@ -6,11 +6,42 @@
 using namespace PostgreSQL;
 using std::endl;
 using std::stringstream;
+using std::to_string;
 using strutils::append;
 
 extern Server g_server;
 
 namespace citefind {
+
+void check_for_empty_titles() {
+  LocalQuery q("*", "citation.works", "title = ''");
+  if (q.submit(g_server) != 0) {
+    append(myoutput, "  **Error checking for empty titles: '" + q.error() +
+        "'", "\n");
+  } else {
+    append(myoutput, "  # works without a title: " + to_string(q.num_rows()),
+        "\n");
+  }
+}
+
+void check_author_names() {
+  LocalQuery q("*", "citation.works_authors", "last_name like '%\\-%'");
+  if (q.submit(g_server) != 0) {
+    append(myoutput, "  **Error checking for author last names with escaped "
+        "hyphen: '" + q.error() + "'", "\n");
+  } else {
+    append(myoutput, "  # author last names with an escaped hyphen: " +
+        to_string(q.num_rows()), "\n");
+  }
+  q.set("*", "citation.works_authors", "last_name like '%\\'%'");
+  if (q.submit(g_server) != 0) {
+    append(myoutput, "  **Error checking for author last names with escaped "
+        "apostrophe: '" + q.error() + "'", "\n");
+  } else {
+    append(myoutput, "  # author last names with an escaped apostrophe: " +
+        to_string(q.num_rows()), "\n");
+  }
+}
 
 void print_publisher_list() {
   LocalQuery q("distinct publisher", "citation.works");
@@ -25,6 +56,8 @@ void print_publisher_list() {
 }
 
 void run_db_integrity_checks() {
+  check_for_empty_titles();
+  check_author_names();
   print_publisher_list();
 }
 
